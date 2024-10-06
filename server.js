6ceca36a380a4079d0776e02e2d2d04ec01e996e
Nanor-0196-Bright-415-Nanor <147738677+Nanor-0196-Bright-415-Nanor@users.noreply.github.com>
2024-10-06 23:21:39 +0000

    introduction to node js,first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+    // Difference Between Node Js and Javascript.
+    -The console is the terminal Window
+    - Global Instead of window object 
+    - When importing , we use "require"
+    -CommonJs modules instead of ES6 modules.
+    
+    console.log("Hello World")
+    const me = require("os")
+    //Os operating system.
+    console.log(me.homedir())
+    console.log(me.networkInterfaces())
+    console.log(me.hostname())
+    console.log(me.type())
+    console.log(me.userInfo())
+    console.log(me.version())
+    console.log(me.machine())
+//
